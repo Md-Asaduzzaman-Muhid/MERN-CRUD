5fe60d858003399181dd93a84694bca6f76e880a
MD-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-05-12 05:37:33 +0600

    crud route added

--- mern-crud-app/backend/models/exercise.model.js
+++ mern-crud-app/backend/models/exercise.model.js
@@ -5,22 +5,22 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema({
     username:{
         type: String,
-        required: true
+        required: true,
     },
     description:{
         type: String,
-        required: true
+        required: true,
     },
     duration:{
         type: Number,
-        required: true
+        required: true,
     },
     date:{
         type: Date,
-        required: true
+        required: true,
     }
 },{
     timestamps: true,
 });
-const Exercise = mongoose.model('Eercise',exerciseSchema);
+const Exercise = mongoose.model('Exercise',exerciseSchema);
 module.exports = Exercise;
--- mern-crud-app/backend/server.js
+++ mern-crud-app/backend/server.js
@@ -23,6 +23,12 @@ connection.once('open',()=>{
     console.log("Mongo DB Connected");
 });
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 app.listen( port, () =>{
     console.log(`Example app listening at http://localhost: ${port}`)
 });
