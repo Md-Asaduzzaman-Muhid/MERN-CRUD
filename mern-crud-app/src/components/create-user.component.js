68c38a00eaa2dc5bb6d76986584698cbdc634850
MD-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-05-13 21:55:47 +0600

    exercise done

--- mern-crud-app/src/components/create-user.component.js
+++ mern-crud-app/src/components/create-user.component.js
@@ -1,11 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class CreateUser extends Component{
-    render(){
-        return(
-            <div>
-                <p>User List component</p>
-            </div>
-        )
+export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: ''
     }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
+    }
+
+    console.log(user);
+
+    axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      username: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3 className="text-center">Create New User</h3>
+        <div className="container">
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
+        </div>
+      </div>
+    )
+  }
 }
--- mern-crud-app/src/components/navbar.component.js
+++ mern-crud-app/src/components/navbar.component.js
@@ -4,24 +4,27 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component{
     render(){
         return(
-            <nav className = "navbar navbar-dark bg-dark navbar-expand-lg" >
-                <Link to ="/" className = "navbar-brand">ToDo Track</Link>
-                <div className="collapse navbar-collapse">
-                    <ul className = "navbar-nav mr-auto">
-                        <li className="navbar-item">
-                            <Link to="/" className="nav-link">Home</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/create" className="nav-link">Create Todo</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/user" className="nav-link">Create User</Link>
-                        </li>
+            <header className="header bg-dark ">
+            <div className= "container">
+                <nav className = "navbar navbar-dark navbar-expand-lg" >
+                    <Link to ="/" className = "navbar-brand">ToDo Track</Link>
+                    <div className="collapse navbar-collapse">
+                        <ul className = "navbar-nav mr-auto">
+                            <li className="navbar-item">
+                                <Link to="/" className="nav-link">Home</Link>
+                            </li>
+                            <li className="navbar-item">
+                                <Link to="/create" className="nav-link">Create Todo</Link>
+                            </li>
+                            <li className="navbar-item">
+                                <Link to="/user" className="nav-link">Create User</Link>
+                            </li>
 
-                    </ul>
-                </div>
-
-            </nav>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            </header>
         );
     }
 }
